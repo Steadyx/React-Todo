d297432838a97ac8bccfae746c25803ad858cc69
Steadyx <edwardlanekemp@gmail.com>
2017-08-08 01:00:48 +0100

    added two more components for future development

--- src/scripts/components/taskItem.js
+++ src/scripts/components/taskItem.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Checked from './taskChecked';
 const TaskItem = props => {
 	console.log(props);
-	let id = 0;
 	const checklistItems = props.createItemList.map((values, index) => {
-		id++;
 		return (
 			<div className="check-list-row" key={index}>
 				<Checked />
